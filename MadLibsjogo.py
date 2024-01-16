c698070c47e97b787ade0c44028ee5fdb1c69dae
Luliima <lucsliimads15@gmail.com>
2024-01-16 12:23:40 -0300

    mudei sol pra chuva

--- MadLibsjogo.py
+++ MadLibsjogo.py
@@ -3,5 +3,5 @@ substantivo = input ('digite seu substantivo')
 cor = input (' digite sua cor ')
 verbo = input (' digite seu verbo')
 adjetivo = input ('digite seu adjetivo')
-HistoriaFormatada = f' Um dia de sol, sai com a camiseta da cor{cor}.Porem sentir muito {verbo},Mas mesmo assim suando muito fique me sentindo {adjetivo} .me fez o favor de me dar um ventilador para amenizar o calor {substantivo}.'
+HistoriaFormatada = f' Um dia de chuva, sai com a camiseta da cor{cor}.Porem sentir muito {verbo},Mas mesmo assim suando muito fique me sentindo {adjetivo} .me fez o favor de me dar um ventilador para amenizar o calor {substantivo}.'
 print (HistoriaFormatada)
