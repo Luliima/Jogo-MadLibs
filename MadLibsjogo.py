ce2ce21b560e10e0343c5f166823e97fdf707c10
Luliima <156527588+Luliima@users.noreply.github.com>
2024-01-16 11:59:19 -0300

    Add files via upload

---
+++ MadLibsjogo.py
@@ -0,0 +1,7 @@
+print ("Olá, bem vindo ao jogo dos Ocdp")
+substantivo = input ('digite seu substantivo')
+cor = input (' digite sua cor ')
+verbo = input (' digite seu verbo')
+adjetivo = input ('digite seu adjetivo')
+HistoriaFormatada = f' Um dia de sol, sai com a camiseta da cor{cor}.Porem sentir muito {verbo},Mas mesmo assim suando muito fique me sentindo {adjetivo} .me fez o favor de me dar um ventilador para amenizar o calor {substantivo}.'
+print (HistoriaFormatada)
